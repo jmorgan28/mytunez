807368329b119355cf7fcce868c3bc2f7851cc0b
Jackson Morgan <jmorgan@stuy.edu>
2016-10-14 11:18:06 -0400

    copied stuff

---
+++ .#mytunez.c
@@ -0,0 +1,1 @@
+jackson.morgan@cslab1-12.3367:1475861432
---
+++ mytunez.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+typedef struct song_node{ 
+  char name[256];
+  char artist[256];
+  struct song_node *next;
+} song_node;
+
+
+int main(){
+  return 0;
+}
+
+
