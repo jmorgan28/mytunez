d414c7d1cba478cc72c2faf8d54799b5ecf27f52
Jackson Morgan <jmorgan@stuy.edu>
2016-10-15 22:47:59 -0400

    insertfront done

--- mytunez.c
+++ mytunez.c
@@ -19,20 +19,22 @@ void print_list(struct song_node *n){
   printf("[ ");
   while(n){
     printf("%s-", n->name);
-    printf("%s ", n->artist);
+    printf("%s, ", n->artist);
     n = n->next;
   }
   printf("]\n");
 }
 
-/*struct node * insert_front(struct node *n, int i){
-  struct node *temp;
-  temp = (struct node *) malloc(sizeof(struct node));
-  temp->i = i;
+struct song_node * insert_front(struct node *n, char s[256], char a[256] ){
+  struct song_node *temp;
+  temp = (struct song_node *) malloc(sizeof(struct song_node));
+  strcpy(temp->name,s);
+  strcpy(temp->artist,a);
   temp->next = n;
   return temp;
 }
- 
+
+/*
 struct node * free_list(struct node *n){
   struct node *temp;
   temp = (struct node *) malloc(sizeof(struct node));
@@ -54,11 +56,12 @@ int main(){
   f = (struct song_node *) malloc(sizeof(struct song_node));
   //char e[256] = "God only knows"; 
   //char d[256] = "Beach Boys";
-  //strcpy(f->name, "God only knows");
-  //strcpy(f->artist, "Beach Boys");
-  struct song_node *g;
-  g = (struct song_node *) malloc(sizeof(struct song_node));
+  strcpy(f->name, "God only knows");
+  strcpy(f->artist, "Beach Boys");
+  //struct song_node *g;
+  // g = (struct song_node *) malloc(sizeof(struct song_node));
   print_list(f);
+  print_list(insert_front(f,"Girl", "Beatles"));
   // f->next = g;
   return 0;
 }
