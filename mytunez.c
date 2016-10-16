28e159131cd48707e90534fca32ca550baf76af3
Jackson Morgan <jmorgan@stuy.edu>
2016-10-16 14:57:06 -0400

    save

--- mytunez.c
+++ mytunez.c
@@ -25,17 +25,53 @@ void print_list(struct song_node *n){
   printf("]\n");
 }
 
-struct song_node * insert_front(struct node *n, char s[256], char a[256] ){
+char * lower(char *p){
+  while(*p){
+    if( (*p >= 65) && (*p <= 90)){
+      *p = *p + 32;
+  
+	  }
+  
+    p ++;
+    //printf("%s\n", *p);
+  }
+  
+  return p;	
+}
+
+struct song_node * insert_front(struct song_node *n, char s[256], char a[256] ){
   struct song_node *temp;
   temp = (struct song_node *) malloc(sizeof(struct song_node));
+  lower(s);
   strcpy(temp->name,s);
   strcpy(temp->artist,a);
   temp->next = n;
   return temp;
 }
 
+struct song_node * insert_order(struct song_node *n, char s[256], char a[256]){
+  struct song_node *temp;
+  temp = (struct song_node *) malloc(sizeof(struct song_node));
+  strcpy(temp->name,s);
+  strcpy(temp->artist,a);
+  lower(temp->name);
+  if(strcmp(n->name, temp->name) > 0){
+    // printf("you \n");
+    temp->next = n;
+    return temp;
+  }
 
-struct song_node * free_list(struct song_node *n){
+
+  //printf("you \n");
+  //while((strcmp((lower(n->name)), (lower(temp->name)))) > 0){
+  //n = n->next;
+
+  //}
+  return n;
+}
+
+
+struct song_node * free_list(struct song_node *n){ 
   struct song_node *temp;
   temp = (struct song_node *) malloc(sizeof(struct song_node));
   temp = n;
@@ -54,16 +90,19 @@ struct song_node * free_list(struct song_node *n){
 int main(){
   struct song_node *f;
   f = (struct song_node *) malloc(sizeof(struct song_node));
-  //char e[256] = "God only knows"; 
+  char e[256] = "God only knows"; 
   //char d[256] = "Beach Boys";
-  strcpy(f->name, "God only knows");
-  strcpy(f->artist, "Beach Boys");
+  //strcpy(f->name, "God only knows");
+  //strcpy(f->artist, "Beach Boys");
+  f = insert_front(f, e, "beach boys");
   //struct song_node *g;
   // g = (struct song_node *) malloc(sizeof(struct song_node));
-  print_list(f);
-  print_list(free_list(insert_front(f,"Girl", "Beatles")));
+  //print_list(insert_front(f, "that" ,"so"));
+  print_list(insert_order(f,"A day in the life", "Beatles"));
   // f->next = g;
   //print_list(f);
+  //lower(e);
+  //printf("%s \n", e);
   return 0;
 }
 
