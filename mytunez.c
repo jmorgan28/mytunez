cac1f4fc86f28b248107b1d80e5ec42432b2c004
Jackson Morgan <jmorgan@stuy.edu>
2016-10-15 22:41:51 -0400

    I guess I did something

--- mytunez.c
+++ mytunez.c
@@ -8,32 +8,24 @@ typedef struct song_node{
   struct song_node *next;
 } song_node;
 
-song_node *table[26];
+//song_node *table[26];
 
 
-struct node {int i; struct node *next;};
-
-
-void print_list(struct node *n){
-  if(n == NULL){
+void print_list(struct song_node *n){
+  if(n == NULL || n == 0){
     printf("[]");
     return;
   }
-  struct node *temp;
-  temp = (struct node *) malloc(sizeof(struct node));
-  temp = n;
   printf("[ ");
-  printf("%d -", song_node->name);
-  printf("%d, ", song_node->artists)
-  while((*temp).next != NULL){
-    temp = (*temp).next;
-    printf("%d -", song_node->name);
-    printf("%d, ", song_node->artists)
+  while(n){
+    printf("%s-", n->name);
+    printf("%s ", n->artist);
+    n = n->next;
   }
   printf("]\n");
 }
 
-struct node * insert_front(struct node *n, int i){
+/*struct node * insert_front(struct node *n, int i){
   struct node *temp;
   temp = (struct node *) malloc(sizeof(struct node));
   temp->i = i;
@@ -55,9 +47,19 @@ struct node * free_list(struct node *n){
   n= NULL;
   return n;
 }
-
+*/
 
 int main(){
+  struct song_node *f;
+  f = (struct song_node *) malloc(sizeof(struct song_node));
+  //char e[256] = "God only knows"; 
+  //char d[256] = "Beach Boys";
+  //strcpy(f->name, "God only knows");
+  //strcpy(f->artist, "Beach Boys");
+  struct song_node *g;
+  g = (struct song_node *) malloc(sizeof(struct song_node));
+  print_list(f);
+  // f->next = g;
   return 0;
 }
 
